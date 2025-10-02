30b40c66df222f487a939f467125edbf02a29c11
Nurayn Guru <gnurayn@gmail.com>
2025-10-02 12:32:26 -0400

    a2 CSS and Bootstrap 3

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -3,6 +3,7 @@ export default function Labs() {
  return (
    <div id="wd-labs">
      <h1>Labs</h1>
+     <h2>Nurayn Guru - Online Section</h2>
      <ul>
        <li>
          <Link href="/Labs/Lab1" id="wd-lab1-link">
