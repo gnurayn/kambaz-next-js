57ee144685ca5416cecf85c90a9e9c26cd5ea18e
Nurayn Guru <gnurayn@gmail.com>
2025-11-02 20:02:46 -0500

    please i want to be done with a4

--- app/Labs/Lab4/ReduxExamples/page.tsx
+++ app/Labs/Lab4/ReduxExamples/page.tsx
@@ -1,17 +1,22 @@
 "use client";
+import { Provider } from "react-redux";
 import AddRedux from "./AddRedux";
 import CounterRedux from "./CounterRedux";
 import HelloRedux from "./HelloRedux";
 import TodoList from "./todos/TodoList";
+import store from "@/store/labstore";
 export default function ReduxExamples() {
     return (
-        <div>
-            <h2>Redux Examples</h2>
-            <HelloRedux />
-            <CounterRedux />
-            <AddRedux />
-            <TodoList />
-        </div>
+        <Provider store={store}>
+
+            <div>
+                <h2>Redux Examples</h2>
+                <HelloRedux />
+                <CounterRedux />
+                <AddRedux />
+                <TodoList />
+            </div>
+        </Provider>
     );
 };
 
