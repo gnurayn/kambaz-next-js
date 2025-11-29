5538da6c5d99b0e43a60cde40ba84812651fbd19
gnurayn <149106331+gnurayn@users.noreply.github.com>
2025-11-29 14:57:50 -0500

    a6

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -2,10 +2,14 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useSelector } from "react-redux";
 import "../styles.css";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 export default function AccountNavigation() {
   const pathname = usePathname();
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
 
   const links = [
     { href: "/Account/Signin", label: "Signin" },
@@ -25,7 +29,16 @@ export default function AccountNavigation() {
           {link.label}
         </Link>
       ))}
+
+      {currentUser && currentUser.role === "ADMIN" && (
+        <Link
+          href="/Account/Users"
+          className={`list-group-item border-0 ${pathname.endsWith("Users") ? "active" : ""
+            }`}
+        >
+          Users
+        </Link>
+      )}
     </div>
   );
 }
-
--- app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
+++ app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
@@ -1,27 +1,42 @@
 "use client"
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
-import React from "react";
-import { useParams } from "next/navigation";
-import * as db from "../../../../Database";
-export default function PeopleTable() {
-  const { cid } = useParams();
-  const { users, enrollments } = db;
+import React, { useState } from "react";
+import PeopleDetails from "../Details";
+import Link from "next/link"
+import * as client from "../../../../Account/client";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export default function PeopleTable({ users = [], fetchUsers }: { users?: any[]; fetchUsers: () => void; }) {
+  const [showDetails, setShowDetails] = useState(false);
+  const [showUserId, setShowUserId] = useState<string | null>(null);
   return (
     <div id="wd-people-table">
+      {showDetails && (
+        <PeopleDetails
+          uid={showUserId}
+          onClose={() => {
+            setShowDetails(false);
+            fetchUsers();
+          }} />
+      )}
       <Table>
         <tbody>
           {users
-            .filter((usr) =>
-              enrollments.some((enrollment) => enrollment.user === usr._id && enrollment.course === cid)
-            )
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             .map((user: any) => (
               <tr key={user._id}>
                 <td className="wd-full-name text-nowrap">
-                  <FaUserCircle className="me-2 fs-1 text-secondary" />
-                  <span className="wd-first-name">{user.firstName}{'\u00A0'}</span>
-                  <span className="wd-last-name">{user.lastName}</span>
+                  <span className="text-decoration-none"
+                    onClick={() => {
+                      setShowDetails(true);
+                      setShowUserId(user._id);
+                    }} >
+                    <FaUserCircle className="me-2 fs-1 text-secondary" />
+                    <span className="wd-first-name">{user.firstName}{'\u00A0'}</span>
+                    <span className="wd-last-name">{user.lastName}</span>
+                  </span>
                 </td>
                 <td className="wd-login-id">{user.loginId}</td>
                 <td className="wd-section">{user.section}</td>
