f26ba168c9434cccad41d607faa022e5853037a1
gnurayn <149106331+gnurayn@users.noreply.github.com>
2025-12-03 14:25:31 -0500

    ugh

--- app/(Kambaz)/Courses/[cid]/Quizzes/QuizListItem.tsx
+++ app/(Kambaz)/Courses/[cid]/Quizzes/QuizListItem.tsx
@@ -2,7 +2,7 @@
 import { useRouter } from "next/navigation";
 import { Dropdown } from "react-bootstrap";
 import { useAuth } from "../../../Account/useAuth";
-import "./style.css";
+import "../../../style.css";
 
 interface QuizListItemProps {
     quiz: any;
